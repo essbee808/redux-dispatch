da47c8960512036a3a7e71da2ca3de510cebcecb
Sheena Bonilla <sheena.myb@gmail.com>
2022-08-07 19:24:13 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,6 @@
+let state = {count: 0};
+
+// reducer
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +10,20 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
+// persist changes to our state
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render() {
+  document.body.textContent = state.count;
+}
+
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+dispatch({type: "INCREASE_COUNT"});
+dispatch({type: "INCREASE_COUNT"});
+dispatch({type: "INCREASE_COUNT"});
+
+render();
